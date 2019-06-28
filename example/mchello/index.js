3fcd8457b3965cea2e337c291706e3ddcbf93473
motebus <32254088+motebus@users.noreply.github.com>
2019-06-28 01:29:29 -0400

    Add files via upload

--- example/mchello/index.js
+++ example/mchello/index.js
@@ -1,13 +1,14 @@
 // motechat: 
-// Date: 2018/10/12
+// Date: 2019/6/28
 // 
 //var fs = require('fs');
 //var conf = JSON.parse(fs.readFileSync(__dirname +'/config/config.json', 'utf8'));
 var conf = require('./conf/config.json');
 console.log('conf=%s',JSON.stringify(conf));
-if ( process.env.AppName ) conf.AppName = process.env.AppName;
-if ( process.env.DCenter ) conf.DCenter = process.env.DCenter;
+if ( process.env.APP_NAME ) conf.AppName = process.env.APP_NAME;
+if ( process.env.DC ) conf.DCenter = process.env.DC;
 if ( process.env.IOC ) conf.IOC = process.env.IOC;
+if ( process.env.MOTEBUS_GATEWAY ) conf.MotebusGW = process.env.MOTEBUS_GATEWAY;
 
 mc = require('./app/appmain.js');
 mc.Start( null, conf );
--- example/mchello/mchello.js
+++ example/mchello/mchello.js
@@ -5,9 +5,10 @@
 //var conf = JSON.parse(fs.readFileSync(__dirname +'/config/config.json', 'utf8'));
 var conf = require('./conf/config.json');
 console.log('conf=%s',JSON.stringify(conf));
-if ( process.env.AppName ) conf.AppName = process.env.AppName;
-if ( process.env.DCenter ) conf.DCenter = process.env.DCenter;
+if ( process.env.APP_NAME ) conf.AppName = process.env.APP_NAME;
+if ( process.env.DC ) conf.DCenter = process.env.DC;
 if ( process.env.IOC ) conf.IOC = process.env.IOC;
+if ( process.env.MOTEBUS_GATEWAY ) conf.MotebusGW = process.env.MOTEBUS_GATEWAY;
 
 mc = require('./app/appmain.js');
 mc.Start( null, conf );
